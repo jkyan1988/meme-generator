406b2e6ce27212bcd79480bbfe8601d07f82810d
James Kyan <103395620+jkyan1988@users.noreply.github.com>
2022-06-03 16:33:42 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,129 +1,47 @@
-import {key} from '/secret.js'
 
-document.addEventListener('DOMContentLoaded', () => {
-console.log('this is my key', key)
-//FETCH FUNCTION API / API KEY
-
-
-
-
-//FOOD LIST ELEMENTS
-
-const foodListMain = document.getElementById('food-list')
-const foodListList = document.getElementById('foodlistlist')
-const foodList = document.getElementById("list-food")
-const foodListB = document.getElementById("list-food-b")
-const foodListC = document.getElementById("list-food-c")
-const foodListD = document.getElementById("list-food-d")
-const foodListE = document.getElementById("list-food-e")
-
-
-
-//FOOD FORM ELEMENTS
-
-const foodForm = document.getElementById('food-form')
-
-let foodFormInput = document.getElementById('food-input')
-let foodFormInputB = document.getElementById('food-input-b')
-let foodFormInputC = document.getElementById('food-input-c')
-let foodFormInputD = document.getElementById('food-input-d')
-let foodFormInputE = document.getElementById('food-input-e')
-
-
-//FOOD NUTRITION GUIDE ELEMENTS
-
-const nutritionGuide = document.getElementById('nutrition-guide')
-const foodCalories = document.getElementById('calories-id')
-const foodFat = document.getElementById('fat-id')
-const foodProtein = document.getElementById('protein-id')
-const foodCarb = document.getElementById('carb-id')
-const foodPrice = document.getElementById('price-id')
-	
-// FOOD PIE CHART
-
-const pieChart = document.getElementById('pie-chart')
-
-
-//FOOD EVENT LISTENERS - FOOD LIST
-
-foodListMain.addEventListener('mouseover', foodListMouseOver)
-
-function foodListMouseOver(event) {
-	event.target.style.color = "orange"
-}
-
-foodListList.addEventListener('mouseover', foodListListMouseOver)
-
-function foodListListMouseOver(event) {
-	event.target.style.color = "blue"
-}
-
-//FOOD EVENT LISTENERS - NUTRITION GUIDE
-
-foodCalories.addEventListener('click', calorieClick)
-
-function calorieClick(event) {
-	foodCalories.innerText = foodFormInput.value
-}
-
-foodFat.addEventListener('click', fatClick)
-
-function fatClick(event) {
-	foodFat.innerText = foodFormInputB.value
-}
-
-foodProtein.addEventListener('click', proteinClick)
-
-function proteinClick(event) {
-	foodProtein.innerText = foodFormInputC.value
-}
-
-foodCarb.addEventListener('click', carbClick)
-
-function carbClick(event) {
-	foodCarb.innerText = foodFormInputD.value
-}
-
-foodPrice.addEventListener('click', priceClick)
-
-function priceClick(event) {
-	foodPrice.innerText = foodFormInputE.value
+function makeMyMeme(object){
+    fetch('https://api.imgflip.com/get_memes')
+    .then(response => response.json())
+    .then(randomMemes => {
+        const memesName = randomMemes.data.memes[object].name
+        document.getElementById("label-meme").innerText = memesName
+        document.getElementById("meme-id").innerText = object
+        const memesImage = randomMemes.data.memes[object].url
+        document.getElementById("meme-pic").setAttribute('src', `${memesImage}`)
+    })
 }
 
 
-//FOOD EVENT LISTENERS - FOOD FORM
 
-foodForm.addEventListener("submit", (event) => {
-		event.preventDefault()
-		foodList.textContent = foodFormInput.value
-		foodListB.textContent = foodFormInputB.value
-		foodListC.textContent = foodFormInputC.value
-		foodListD.textContent = foodFormInputD.value
-		foodListE.textContent = foodFormInputE.value
+const arrayMemes = [0,1,2,3,4,5,6,7,8,9]
+const addNumber = arrayMemes.map((num) => (num * num))
 
-})
 
-//FOOD EVENT LISTENERS - PIE CHART
+makeMyMeme(addNumber[1])
 
-pieChart.addEventListener('mouseover', pieChartChange)
-
-function pieChartChange(event) {
-	event.target.style.color = "green"
-}
-
-// DO NOT DELETE BELOW THE `})` part of DOMContentloaded and side Menu Div
+const makeButton = document.getElementById('meme-button')
+makeButton.addEventListener('click', () => {
+    let createNumber = Math.floor(Math.random() *88)
+    makeMyMeme(createNumber)
 })
 
 
+const memeTitle = document.getElementById('meme-title')
+memeTitle.addEventListener('mouseover', changeColorByHover)
+function changeColorByHover(event) {
+    memeTitle.style.color = 'blue'
+}
 
-/* Open when someone clicks on the span element */
-function openNav() {
-	document.getElementById("side-nav").style.width = "50%";
+const memeSubtitle = document.getElementById('meme-subtitle')
+memeSubtitle.addEventListener('dblclick', (e) => {
+    alert("Welcome to my Meme Page")
+  })
+
+window.addEventListener('resize', resizeWindow)
+function resizeWindow(){
+    alert('Window has been randomized as well!')
 }
-		  
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-	document.getElementById("side-nav").style.width = "0%";
-}
-	
+
+
+
 
