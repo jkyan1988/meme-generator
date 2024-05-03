9c28180e9c538d6c5fefa41e4e2460c0c0cf3fa9
jkyan1988 <james.kyan88@gmail.com>
2024-05-03 16:00:49 -0400

    starting over from scratch

--- index.js
+++ index.js
@@ -1,44 +1,4 @@
-
-function makeMyMeme(object){
-    fetch('https://api.imgflip.com/get_memes')
-    .then(response => response.json())
-    .then(randomMemes => {
-        const memesName = randomMemes.data.memes[object].name
-        document.getElementById("label-meme").innerText = memesName
-        document.getElementById("meme-id").innerText = object
-        const memesImage = randomMemes.data.memes[object].url
-        document.getElementById("meme-pic").setAttribute('src', `${memesImage}`)
-    })
-}
-
-const arrayMemes = [0,1,2,3,4,5,6,7,8,9]
-const addNumber = arrayMemes.map((num) => (num * num))
-
-
-makeMyMeme(addNumber[1])
-
-const makeButton = document.getElementById('meme-button')
-makeButton.addEventListener('click', () => {
-    let createNumber = Math.floor(Math.random() *88)
-    makeMyMeme(createNumber)
-})
-
-
-const memeTitle = document.getElementById('meme-title')
-memeTitle.addEventListener('mouseover', changeColorByHover)
-function changeColorByHover(event) {
-    memeTitle.style.color = 'blue'
-}
-
-const memeSubtitle = document.getElementById('meme-subtitle')
-memeSubtitle.addEventListener('dblclick', (e) => {
-    alert("Welcome to my Meme Page")
-  })
-
-// window.addEventListener('resize', resizeWindow)
-// function resizeWindow(){
-//     alert('Window has been randomized as well!')
-// }
+// start from scratch
 
 
 
